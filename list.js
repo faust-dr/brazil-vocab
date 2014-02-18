129a788335982501482cad7dd449c9d4bfe14a31
Dr. Faust <sinaloas@yandex.com>
2014-02-17 18:17:44 -0800

    Add words

--- list.js
+++ list.js
@@ -2,7 +2,7 @@ numbers1 = [
 	{ english: '0', port: 'zero' },
 	{ english: '1', port: ['um', 'uma'] },
 	{ english: '2', port: ['dois', 'duas'] },
-	{ english: '3', port: 'tres' },
+	{ english: '3', port: 'três' },
 	{ english: '4', port: 'quatro' },
 	{ english: '5', port: 'cinco' },
 	{ english: '6', port: 'seis' },
@@ -11,10 +11,120 @@ numbers1 = [
 	{ english: '9', port: 'nove' },
 	{ english: '10', port: 'dez' }
 ]
+
+numbers2 = [
+	{ english: '11', port: 'onze' },
+	{ english: '12', port: 'doze' },
+	{ english: '13', port: 'treze' },
+	{ english: '14', port: 'quatorze' },
+	{ english: '15', port: 'quinze' },
+	{ english: '16', port: 'dezesseis' },
+	{ english: '17', port: 'dezessete' },
+	{ english: '18', port: 'dezoito' },
+	{ english: '19', port: 'dezenove' },
+	{ english: '20', port: 'vinte' },
+]
+
+numbers3= [
+	{ english: '21', port: 'vinte u um' },
+	{ english: '22', port: 'vinte u dois' },
+	{ english: '23', port: 'vinte e três' },
+	{ english: '24', port: 'vinte e quatro' },
+	{ english: '25', port: 'vinte e cinco' },
+	{ english: '26', port: 'vinte e seis' },
+	{ english: '27', port: 'vinte e sete' },
+	{ english: '28', port: 'vinte e oito' },
+	{ english: '29', port: 'vinte e nove' },
+]
+
+numbers4 = [
+	{ english: '30', port: 'trinta' },
+	{ english: '40', port: 'quarenta' },
+	{ english: '50', port: 'cinquenta' },
+	{ english: '60', port: 'sessenta' },
+	{ english: '70', port: 'setenta' },
+	{ english: '80', port: 'oitenta' },
+	{ english: '90', port: 'noventa' },
+	{ english: '100', port: 'cem' },
+]
+
+numbers5 = [
+	{ english: '200', port: 'duzentos' },
+	{ english: '300', port: 'trezentos' },
+	{ english: '400', port: 'quatrocentos' },
+	{ english: '500', port: 'quinhentos' },
+	{ english: '600', port: 'seicentos' },
+	{ english: '700', port: 'setecentos' },
+	{ english: '800', port: 'oitocentos' },
+	{ english: '900', port: 'novecentos' },
+	{ english: '1000', port: 'mil' },
+]
+
+years = [
+	{ english: '1995', port: 'Mil novecentos e noventa e cinco' },
+	{ english: '1982', port: 'Mil novecentos e oitenta e dois' },
+	{ english: '2014', port: 'dois mil e quatorze' },
+	{ english: '2015', port: 'dois mil e quinze' },
+]
+
+greetings = [
+	{ english: 'Good morning', port: 'Bom dia' },
+	{ english: 'Good afternoon', port: 'Boa tarde' },
+	{ english: 'Good night', port: 'Boa noite' },
+	{ english: 'Everything good?', port: 'Tudo bem?' },
+	{ english: 'Everything is great', port: 'Tudo ótimo' },
+]
+
+comparing = [
+	{ english: 'more', port: 'mais' },
+	{ english: 'less', port: 'menos' },
+]
+
+nouns = [
+	{ english: 'the pen', port: 'a caneta' },
+	{ english: 'the pencil', port: 'o lápis' },
+	{ english: 'the house', port: 'a casa' },
+	{ english: 'the computer', port: 'o computador' },
+]
+
+verbs = [
+	{ english: 'to remember', port: 'lembrar' },
+]
+
+questions = [
+	{ english: 'how much?', port: 'quanto?' },
+	{ english: 'when?', port: 'quando?' },
+	{ english: 'what?', port: 'qual?' },
+	{ english: 'where?', port: 'onde?' },
+	{ english: 'who?', port: 'quem?' },
+]
+
+possessive = [
+	{ english: 'my friend', port: 'Meu amigo' },
+	{ english: 'my jeans', port: 'Minha calça jeans' },
+	{ english: 'my problems', port: 'Meus problemas' },
+	{ english: 'my clothes', port: 'Minhas roupas' },
+	{ english: 'your', port: ['seus', 'suas'] },
+]
+
+time = [ 
+	{ english: 'always', port: 'sempre' },
+]
+
 // template = [
 // 	{ english: "", thai: "", pronunciation: "" },
 // ]
 
 everyLesson = [
 	{ name: "Numbers 1-10", contents: numbers1 },
+	{ name: "Numbers 11-20", contents: numbers2 },
+	{ name: "Numbers 21-29", contents: numbers3 },
+	{ name: "Numbers 30-100", contents: numbers4 },
+	{ name: "Numbers 200-1000", contents: numbers5 },
+	{ name: "Greetings", contents: greetings },
+	{ name: "Nouns", contents: nouns },
+	{ name: "Verbs", contents: verbs },
+	{ name: "Questions", contents: questions },
+	{ name: "Comparing", contents: comparing },
+	{ name: "Possessive", contents: possessive },
 ]

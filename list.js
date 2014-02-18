37c91caf978309097653676a4d6cc1d536863d80
Dr. Faust <sinaloas@yandex.com>
2014-02-18 00:29:48 -0800

    More words

--- list.js
+++ list.js
@@ -85,10 +85,24 @@ nouns = [
 	{ english: 'the pencil', port: 'o lápis' },
 	{ english: 'the house', port: 'a casa' },
 	{ english: 'the computer', port: 'o computador' },
+	{ english: 'cash register', port: 'caixa' },
+	{ english: 'sister', port: 'irmã' },
+	{ english: 'brother', port: 'irmão' },
+	{ english: 'problem', port: 'problema' },
+	{ english: 'work', port: 'trabalho' },
+	{ english: 'headache', port: 'dor de cabeça' },
+	{ english: 'car', port: 'carro' },
+	{ english: 'bus', port: 'ônibus' },
+	{ english: 'suit', port: 'terno' },
+	{ english: 'shoes', port: 'sapatos' },
+	{ english: 'tie', port: 'gravata' },
+	{ english: 'team', port: 'time' },
+	{ english: 'time', port: 'tempo' },
 ]
 
 verbs = [
 	{ english: 'to remember', port: 'lembrar' },
+	{ english: 'to have', port: 'ter' },
 ]
 
 questions = [
@@ -97,6 +111,9 @@ questions = [
 	{ english: 'what?', port: 'qual?' },
 	{ english: 'where?', port: 'onde?' },
 	{ english: 'who?', port: 'quem?' },
+	{ english: 'Como fala', port: 'How do you say' },
+	{ english: 'Como se diz', port: 'How do we say' },
+	{ english: 'Como fala "Hello" em português?', port: 'How do you say "office" in Portuguese?' },
 ]
 
 possessive = [
@@ -109,6 +126,60 @@ possessive = [
 
 time = [ 
 	{ english: 'always', port: 'sempre' },
+	{ english: 'Now', port: 'Agora' },
+]
+
+locations = [
+	{ english: 'here', port: 'aqui' },
+	{ english: 'there', explanation: 'close by', port: 'ali' },
+	{ english: 'there', explanation: 'far', port: 'lá' },
+]
+
+days = [
+	{ english: 'Monday', port: 'Segunda-feira' },
+	{ english: 'Tuesday', port: 'Terca-feira' },
+	{ english: 'Wednesday', port: 'Quarta-feira' },
+	{ english: 'Thursday', port: 'Quinta-feira' },
+	{ english: 'Friday', port: 'Sexta-feira' },
+	{ english: 'Saturday', port: 'Sábado' },
+	{ english: 'Sunday', port: 'Domingo' },
+]
+
+helpful = [
+	{ english: 'Hi', port: ['Oi', 'Olá'] },
+	{ english: 'Bye', port: 'Tchau' },
+	{ english: 'See you', port: 'Até mais' },
+	{ english: 'See you later', port: 'Até mais tarde' },
+	{ english: 'See you tomorrow', port: 'Até amanhã' },
+	{ english: 'Excuse me', port: 'Com licença' },
+	{ english: 'Pardon', port: 'Perdão' },
+	{ english: 'Please', port: 'Por favor' },
+	{ english: 'Welcome', port: 'Bem-vindo' },
+	{ english: 'Sorry', port: 'Desculpa' },
+	{ english: 'But', port: 'Mas' },
+	{ english: 'Yes', port: 'Sim' },
+	{ english: 'No', port: 'Não' },
+	{ english: 'Thanks', port: 'Obrigado' },
+	{ english: "You're welcome", port: 'De nada' },
+]
+
+people = [
+	{ english: 'I', port: 'Eu' },
+	{ english: 'You', port: 'Você' },
+]
+
+adjectives = [
+	{ english: 'small', port: 'pequeno' },
+	{ english: 'big', port: 'grande' },
+	{ english: 'fast', port: 'rápdio' },
+	{ english: 'slow', port: 'devagar' },
+	{ english: 'pretty', port: 'bonito' },
+	{ english: 'ugly', port: 'feio' },
+	{ english: 'new', port: 'novo' },
+	{ english: 'old', port: 'velho' },
+	{ english: 'good', port: 'bom' },
+	{ english: 'bad', port: 'ruim' },
+	{ english: 'busy', port: 'ocupado' },
 ]
 
 // template = [
@@ -121,10 +192,17 @@ everyLesson = [
 	{ name: "Numbers 21-29", contents: numbers3 },
 	{ name: "Numbers 30-100", contents: numbers4 },
 	{ name: "Numbers 200-1000", contents: numbers5 },
+	{ name: "Years", contents: years },
 	{ name: "Greetings", contents: greetings },
 	{ name: "Nouns", contents: nouns },
 	{ name: "Verbs", contents: verbs },
+	{ name: "Adjectives", contents: adjectives },
 	{ name: "Questions", contents: questions },
 	{ name: "Comparing", contents: comparing },
 	{ name: "Possessive", contents: possessive },
+	{ name: "Helpful words", contents: helpful },
+	{ name: "Locations", contents: locations },
+	{ name: "Days", contents: days },
+	{ name: "Time", contents: time },
+	{ name: "People", contents: people },
 ]
